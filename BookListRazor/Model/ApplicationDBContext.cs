dc9640f2bcc28ef8b57bf8004b00b84e14dba9d4
Brian Donnoe <dunow33@hotmail.com>
2020-03-29 23:56:57 -0400

    initial commit

---
+++ BookListRazor/Model/ApplicationDBContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Model
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DBContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Book { get; set; }
+    }
+}
